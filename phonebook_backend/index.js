fba9cbd23478e326eda190114fd43d2be649eba3
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-19 15:00:29 +0300

    3.2: puhelinluettelon backend step2

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    let currentTime = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${currentTime}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
