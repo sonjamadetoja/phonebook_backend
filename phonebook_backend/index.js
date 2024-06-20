395fce4293fcad0809f44e960533a236f50f8180
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-20 13:13:16 +0300

    3.5: puhelinluettelon backend step5

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "name": "Arto Hellas",
@@ -49,6 +51,21 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
