6e0369f7b5e7b826dc42e6fd99e00e54f7d423f7
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-26 12:41:43 +0300

    3.15 & 3.16: puhelinluettelo ja tietokanta, step 3-4

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+require('dotenv').config()
+app.use(express.static('dist'))
 const Person = require('./models/person')
 const morgan = require('morgan')
 const cors = require('cors')
 
 morgan.token('reqBody', function getData (req, res) { return JSON.stringify(req.body) })
 
+const ErrorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny', {
   skip: function (req, res) { return req.method == "POST" }
@@ -14,8 +24,6 @@ app.use(morgan('tiny', {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody', {
   skip: function (req, res) { return req.method != "POST" }
 }))
-app.use(cors())
-app.use(express.static('dist'))
 
 let persons = [
     {
@@ -40,10 +48,11 @@ let persons = [
     }
   ]
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons =>
         response.json(persons)
     )
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -62,14 +71,15 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!(body.name && body.number)) {
@@ -92,7 +102,10 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
+
+app.use(ErrorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
