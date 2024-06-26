ff864ce9471ed77112071542697561e651b2a162
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-26 15:41:16 +0300

    3.19: puhelinluettelo ja tietokanta, step7

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ morgan.token('reqBody', function getData (req, res) { return JSON.stringify(req.
 const ErrorHandler = (error, request, response, next) => {
     console.log(error.message)
     if (error.name === "CastError") {
-        return response.status(400).send({ error: 'malformatted id' })
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === "ValidationError") {
+      return response.status(400).json({ error: error.message})
     }
     next(error)
 }
